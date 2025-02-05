c117fb86da0ae55836a8f30376f5eb36a24f4668
nicholasmartino <nicholas.martino@hotmail.com>
2025-02-05 00:12:29 -0800

    add more priviledges

--- setup_gcloud.sh
+++ setup_gcloud.sh
@@ -41,6 +41,21 @@ gcloud iam service-accounts add-iam-policy-binding "${SERVICE_ACCOUNT}@${GCP_PRO
   --role="roles/iam.workloadIdentityUser" \
   --member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${GITHUB_REPO}"
 
+gcloud iam service-accounts add-iam-policy-binding "${SERVICE_ACCOUNT}@${GCP_PROJECT_ID}.iam.gserviceaccount.com" \
+  --project="${GCP_PROJECT_ID}" \
+  --role="roles/artifactregistry.writer" \
+  --member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${GITHUB_REPO}"
+
+gcloud iam service-accounts add-iam-policy-binding "${SERVICE_ACCOUNT}@${GCP_PROJECT_ID}.iam.gserviceaccount.com" \
+  --project="${GCP_PROJECT_ID}" \
+  --role="roles/artifactregistry.reader" \
+  --member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${GITHUB_REPO}"
+
+gcloud iam service-accounts add-iam-policy-binding "${SERVICE_ACCOUNT}@${GCP_PROJECT_ID}.iam.gserviceaccount.com" \
+  --project="${GCP_PROJECT_ID}" \
+  --role="roles/run.developer" \
+  --member="principalSet://iam.googleapis.com/${WORKLOAD_IDENTITY_POOL_ID}/attribute.repository/${GITHUB_REPO}"
+
 gcloud iam workload-identity-pools providers describe "${WORKLOAD_PROVIDER}" \
   --project="${GCP_PROJECT_ID}" \
   --location="global" \
