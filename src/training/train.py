b596739c817c3991b466ec0f70e3c71b06c495bf
nicholasmartino <nicholas.martino@hotmail.com>
2025-02-09 22:59:48 -0800

    try import as well

--- src/training/train.py
+++ src/training/train.py
@@ -3,10 +3,11 @@ import tensorflow as tf
 print(tf.__version__)
 print(tf.config.list_physical_devices())
 
-from Trainer import train
 
 if __name__ == "__main__":
     try:
+        from Trainer import train
+
         train()
     except Exception as e:
         print(f"An error occurred: {e}")
